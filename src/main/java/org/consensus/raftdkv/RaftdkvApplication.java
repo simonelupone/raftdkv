17606f55452abdded79e4b2c4af40bd4cf191d7a
simonelupone <simone.lupone@gmail.com>
2025-11-14 18:41:52 +0100

    feat: add @EnableScheduling to set task schedulation

--- src/main/java/org/consensus/raftdkv/RaftdkvApplication.java
+++ src/main/java/org/consensus/raftdkv/RaftdkvApplication.java
@@ -2,7 +2,16 @@ package org.consensus.raftdkv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * Spring documentation
+ * 
+ * @Configuration - is a class-level annotation indicating that an object is a
+ *                source of bean definitions
+ * @EnableScheduling - enable scheduling in the application.
+ */
+@EnableScheduling
 @SpringBootApplication
 public class RaftdkvApplication {
 
